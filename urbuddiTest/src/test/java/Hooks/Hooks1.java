5214338b38ff5884a18b21f5f782d368f9ad5532
veeraswamyK <veeraswamy.kalluri@optimworks.com>
2025-07-21 18:36:18 +0530

    hooks more options

--- urbuddiTest/src/test/java/Hooks/Hooks1.java
+++ urbuddiTest/src/test/java/Hooks/Hooks1.java
@@ -11,6 +11,7 @@ import utils.driverManager;
 import utils.screenshotGenerator;
 
 import java.time.Duration;
+import java.util.UUID;
 
 public class Hooks1 {
 
@@ -19,6 +20,12 @@ public class Hooks1 {
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
+        String os = System.getProperty("os.name").toLowerCase();
+        if (!os.contains("win")) {
+            // Only use --user-data-dir on Linux CI if really needed (else remove it)
+            String userDataDir = "/tmp/chrome-user-data-" + UUID.randomUUID();
+            options.addArguments("--user-data-dir=" + userDataDir);
+        }
 
 
         WebDriver driver = new ChromeDriver();
